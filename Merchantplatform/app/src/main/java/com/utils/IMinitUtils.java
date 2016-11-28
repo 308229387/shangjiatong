2521a9592da40189fa1ea2f61da9981da9a16d53
songyongmeng <songyongmeng@58ganji.com>
2016-11-28 22:01:14 +0800

    modify im log

--- Merchantplatform/app/src/main/java/com/utils/IMinitUtils.java
+++ Merchantplatform/app/src/main/java/com/utils/IMinitUtils.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Process;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.common.gmacs.BuildConfig;
+import com.common.gmacs.core.Gmacs;
 import com.merchantplatform.application.HyApplication;
 
 import static com.tencent.open.utils.Global.getPackageName;
@@ -20,16 +22,25 @@ public class IMInitUtils {
         init();
     }
 
-    private void init(){
-        if (isUIProcess()){
-            SDKInitializer.initialize(application);
+    private void init() {
+        if (isUIProcess()) {
+            SDKInitializer.initialize(application);//百度地图
+            Gmacs.getInstance().initialize(application);//im环境配置
+            configDebugLog();//配置DebugLog
         }
+    }
+
+    private void configDebugLog() {
+        if (BuildConfig.DEBUG)
+            Gmacs.getInstance().setLoggable(true);
+        else
+            Gmacs.getInstance().setLoggable(false);
     }
 
     private boolean isUIProcess() {
         int pid = Process.myPid();
         String processName = "";
-        ActivityManager mActivityManager = (ActivityManager)application. getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager mActivityManager = (ActivityManager) application.getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningAppProcessInfo appProcess : mActivityManager.getRunningAppProcesses()) {
             if (appProcess.pid == pid) {
                 processName = appProcess.processName;
