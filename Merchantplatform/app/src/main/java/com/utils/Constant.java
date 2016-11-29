b2f02021b89f5e0819670ced7cb5567f921fbe0c
wangjianlong <wangjianlong@58ganji.com>
2016-11-29 13:23:12 +0800

    增加WPush相关功能

--- Merchantplatform/app/src/main/java/com/utils/Constant.java
+++ Merchantplatform/app/src/main/java/com/utils/Constant.java
@@ -8,6 +8,8 @@ public class Constant {
     /**
      * buglyId
      */
-    public static String BUGLY_APP_ID = "900060310";
+    public static final String BUGLY_APP_ID = "900060310";
+    public static final String WPUSH_APP_ID= "1007"; //WPush的APP_ID
+    public static final String WPUSH_APP_KEY = "b04RT5u0dyWXjewN"; //WPush的APP_KEY
 
 }
