f68222e29d538d237c30aae984612a285a237367
wangjianlong <wangjianlong@58ganji.com>
2016-12-20 20:23:01 +0800

    接口联调

--- Merchantplatform/app/src/main/java/com/ui/ActionSheet/ActionSheet.java
+++ Merchantplatform/app/src/main/java/com/ui/ActionSheet/ActionSheet.java
@@ -1,4 +1,4 @@
-package com.ui.actionSheet;
+package com.ui.actionsheet;
 
 import android.app.Dialog;
 import android.content.Context;
--- Merchantplatform/app/src/main/java/com/ui/ActionSheet/NormalActionSheet.java
+++ Merchantplatform/app/src/main/java/com/ui/ActionSheet/NormalActionSheet.java
@@ -1,4 +1,4 @@
-package com.ui.actionSheet;
+package com.ui.actionsheet;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
