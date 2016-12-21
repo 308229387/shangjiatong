62cc5d6e4236692d396c4b1c32237374d0674b8e
songyongmeng <songyongmeng@58ganji.com>
2016-12-21 20:10:36 +0800

    add

---
+++ Merchantplatform/librarys/src/main/java/com/Utils/SystemNotification.java
@@ -0,0 +1,73 @@
+package com.Utils;
+
+import java.io.Serializable;
+
+/**
+ * Created by SongYongmeng on 2016/12/21.
+ */
+
+public class SystemNotification implements Serializable {
+    public String content;
+    public int contentType;
+    public String describe;
+    public Long id;
+    public Long sortId;
+    public String title;
+    public int type;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(int contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSortId() {
+        return sortId;
+    }
+
+    public void setSortId(Long sortId) {
+        this.sortId = sortId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
