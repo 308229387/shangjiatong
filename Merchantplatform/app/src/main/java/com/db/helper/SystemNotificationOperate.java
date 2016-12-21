20eb6bb31a5e6963b1b592d5a2b633b11495eff4
songyongmeng <songyongmeng@58ganji.com>
2016-12-21 20:16:05 +0800

    add

---
+++ Merchantplatform/app/src/main/java/com/db/helper/SystemNotificationOperate.java
@@ -0,0 +1,43 @@
+package com.db.helper;
+
+import android.content.Context;
+
+import com.db.dao.CallDetail;
+import com.db.dao.SystemNotificationDetial;
+import com.db.dao.gen.CallDetailDao;
+import com.db.dao.gen.SystemNotificationDetialDao;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+import org.greenrobot.greendao.query.WhereCondition;
+
+import java.util.ArrayList;
+
+/**
+ * Created by SongYongmeng on 2016/12/21.
+ */
+
+public class SystemNotificationOperate {
+    /**
+     * @desc 添加数据至数据库，如果存在，将原来的数据覆盖
+     **/
+    public static void insertOrReplace(Context context, SystemNotificationDetial callDetail) {
+        DbManager.getDaoSession(context).getSystemNotificationDetialDao().insertOrReplace(callDetail);
+    }
+    /**
+     * @desc 按条件返回按时间排序的结果集
+     **/
+    public static ArrayList<SystemNotificationDetial> queryByCondition(Context context, WhereCondition whereCondition, WhereCondition... condMore) {
+        QueryBuilder<SystemNotificationDetial> builder = DbManager.getDaoSession(context).getSystemNotificationDetialDao().queryBuilder();
+        return (ArrayList<SystemNotificationDetial>) builder.where(whereCondition, condMore).orderDesc(SystemNotificationDetialDao.Properties.SortId).list();
+    }
+
+    /**
+     * @desc 按条件返回按时间排序的结果集
+     **/
+    public static ArrayList<SystemNotificationDetial> queryAll(Context context) {
+        QueryBuilder<SystemNotificationDetial> builder = DbManager.getDaoSession(context).getSystemNotificationDetialDao().queryBuilder();
+        return (ArrayList<SystemNotificationDetial>) builder.orderDesc(SystemNotificationDetialDao.Properties.SortId).list();
+    }
+
+
+}
