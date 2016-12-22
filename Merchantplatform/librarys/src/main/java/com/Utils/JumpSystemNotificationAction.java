3c23b7dd567d7df57c5697826724f469f08c7f4b
songyongmeng <songyongmeng@58ganji.com>
2016-12-22 10:14:39 +0800

    add

---
+++ Merchantplatform/librarys/src/main/java/com/Utils/JumpSystemNotificationAction.java
@@ -0,0 +1,19 @@
+package com.Utils;
+
+/**
+ * Created by SongYongmeng on 2016/12/21.
+ */
+
+public class JumpSystemNotificationAction {
+    private String jump;
+
+    public JumpSystemNotificationAction(String jump) {
+        this.jump = jump;
+    }
+
+    public String getJump() {
+        return jump;
+    }
+
+
+}
