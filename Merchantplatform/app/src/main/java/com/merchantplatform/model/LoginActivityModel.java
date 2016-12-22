bb8f5a73e751bc77ddf44170bbbb3e9770b97726
mengzhiyuan <mengzhiyuan@58ganji.com>
2016-12-22 10:29:53 +0800

    注释掉IM

--- Merchantplatform/app/src/main/java/com/merchantplatform/model/LoginActivityModel.java
+++ Merchantplatform/app/src/main/java/com/merchantplatform/model/LoginActivityModel.java
@@ -1,13 +1,11 @@
 package com.merchantplatform.model;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.callback.DialogCallback;
 import com.merchantplatform.R;
@@ -29,8 +27,6 @@ import com.wuba.loginsdk.external.Request;
 import com.wuba.loginsdk.external.SimpleLoginCallback;
 import com.wuba.loginsdk.model.LoginSDKBean;
 import com.wuba.wbpush.Push;
-
-import org.greenrobot.eventbus.EventBus;
 
 import okhttp3.Call;
 import okhttp3.Response;
@@ -56,18 +52,17 @@ public class LoginActivityModel extends BaseModel {
             public void onLogin58Finished(boolean isSuccess, String msg, @Nullable LoginSDKBean loginSDKBean) {
                 super.onLogin58Finished(isSuccess, msg, loginSDKBean);
                 if (isSuccess && loginSDKBean != null) {
-//                    loginPassrotSuccess();
+                    loginPassrotSuccess();
                     UserUtils.setUserId(context, loginSDKBean.getUserId());
-                    new IMLoginUtils(context);
+//                    new IMLoginUtils(context);
                     StringBuilder temp = new StringBuilder();
                     temp.append(UserUtils.getUserId() + "_");
                     temp.append(AppInfoUtils.getIMEI(HyApplication.getApplication()));
                     String a = temp.toString();
-                    Log.i("song",a);
+                    Log.i("song", a);
                     Push.getInstance().binderAlias(a); //绑定/解绑别名:非空串,绑定指定的alias ,空串(“”),解绑alias。
-                    PageSwitchUtils.goToActivity(context, HomepageActivity.class);
+//                    PageSwitchUtils.goToActivity(context, HomepageActivity.class);
                 }
-
                 if (isPassportLoginFail(loginSDKBean)) {
                     context.finish();
                 }
@@ -75,7 +70,7 @@ public class LoginActivityModel extends BaseModel {
         };
     }
 
-    private void loginPassrotSuccess(){
+    private void loginPassrotSuccess() {
         OkHttpUtils.post(Urls.LOGIN)
                 .execute(new localLoginCallback(context));
     }
@@ -102,7 +97,6 @@ public class LoginActivityModel extends BaseModel {
     private boolean isPassportLoginFail(@Nullable LoginSDKBean loginSDKBean) {
         return loginSDKBean != null && loginSDKBean.getCode() == LoginSDKBean.CODE_CANCEL_OPERATION;
     }
-
 
     public void removeOtherActivity() {
         new Handler().postDelayed(new Runnable() {
@@ -143,26 +137,26 @@ public class LoginActivityModel extends BaseModel {
 
         @Override
         public void onResponse(boolean isFromCache, LoginResponse loginResponse, okhttp3.Request request, @Nullable Response response) {
-            if(loginResponse != null){
+            if (loginResponse != null) {
                 LoginSuccess(loginResponse);
             }
         }
 
         @Override
         public void onError(boolean isFromCache, Call call, @Nullable Response response, @Nullable Exception e) {
-            if(e!= null && !TextUtils.isEmpty(e.getMessage())){
-                if(e.getMessage().equals(PPU_UNVALID)){
+            if (e != null && !TextUtils.isEmpty(e.getMessage())) {
+                if (e.getMessage().equals(PPU_UNVALID)) {
                     initLoginErrorDialog(context.getString(R.string.ppu_expired));
-                }else if(e.getMessage().equals(SINGLE_DEVICE_LOGIN)){
+                } else if (e.getMessage().equals(SINGLE_DEVICE_LOGIN)) {
                     initLoginErrorDialog(context.getString(R.string.force_exit));
-                }else{
-                   initLoginErrorDialog(e.getMessage());
+                } else {
+                    initLoginErrorDialog(e.getMessage());
                 }
             }
         }
     }
 
-    private void initLoginErrorDialog(String message){
+    private void initLoginErrorDialog(String message) {
         if (loginErrorDialog != null && loginErrorDialog.isShowing()) {
             loginErrorDialog.dismiss();
         }
@@ -183,9 +177,7 @@ public class LoginActivityModel extends BaseModel {
                 loginErrorDialog.dismiss();
                 context.finish();
             }
-
         });
         loginErrorDialog.show();
     }
-
 }
