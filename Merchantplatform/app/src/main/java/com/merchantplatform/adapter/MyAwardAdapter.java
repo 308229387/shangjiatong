c58a5056bb80a1e7257589f5289902f2bb7168e1
mengzhiyuan <mengzhiyuan@58ganji.com>
2017-02-15 19:40:02 +0800

    我的奖品时间格式

--- Merchantplatform/app/src/main/java/com/merchantplatform/adapter/MyAwardAdapter.java
+++ Merchantplatform/app/src/main/java/com/merchantplatform/adapter/MyAwardAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 
 import com.merchantplatform.R;
 import com.merchantplatform.bean.MyAwardResponse;
+import com.utils.DateUtils;
 import com.xrecyclerview.BaseRecyclerViewAdapter;
 
 import java.util.ArrayList;
@@ -19,7 +20,7 @@ public class MyAwardAdapter extends BaseRecyclerViewAdapter<MyAwardResponse.awar
     @Override
     protected void bindDataToItemView(MyAwardViewHolder myAwardViewHolder, int position) {
         myAwardViewHolder.setText(R.id.tv_prize_name, mList.get(position).getPrize_name())
-                .setText(R.id.tv_win_time, mList.get(position).getWin_time())
+                .setText(R.id.tv_win_time, DateUtils.formatDateTimeToDate(mList.get(position).getWin_time()))
                 .setText(R.id.tv_my_award_describe, mList.get(position).getPrize_describe());
     }
 
